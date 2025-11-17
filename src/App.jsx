bb3de32f616a1ea06f3060244b1879ffa1072d82
Yaschilal <yasiada14@gmail.com>
2025-11-17 20:53:12 +0300

    comment header

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:3000/api/contact', {
+      const res = await fetch('https://portfolio-backend-ejye.onrender.com/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './assets/compononts/header/header.jsx';
+// import Header from './assets/compononts/header/header.jsx';
 import Section1 from './assets/compononts/section1/Section1';
 import HeroSection from './assets/compononts/HeroSection/HeroSection';
 import Section01 from './assets/compononts/section01/Section01';
